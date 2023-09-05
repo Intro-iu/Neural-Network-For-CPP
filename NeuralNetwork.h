10c23169709ad07b8d1e22002de8b404915fff3d
Intro <72384746+yyccc-iu@users.noreply.github.com>
2023-09-05 12:25:31 +0800

    Update NeuralNetwork.h

--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -9,6 +9,9 @@ struct NNwork {
     int L;  int *Siz;
     Matrix *Z, *A, *W, *B;
     Matrix *dZ, *dW, *dB;
+    Matrix *vW, *vB, *sW, *sB;
+
+    int cnt = 0;
 
     NNwork() {
         L = 1;
@@ -20,6 +23,10 @@ struct NNwork {
         dZ = new Matrix[L];
         dW = new Matrix[L];
         dB = new Matrix[L];
+        vW = new Matrix[L];
+        vB = new Matrix[L];
+        sW = new Matrix[L];
+        sB = new Matrix[L];
     }
 
     NNwork(const int &num, std::initializer_list <int> il, const double &l, const double &r) {
@@ -32,12 +39,20 @@ struct NNwork {
         dZ = new Matrix[L];
         dW = new Matrix[L];
         dB = new Matrix[L];
+        vW = new Matrix[L];
+        vB = new Matrix[L];
+        sW = new Matrix[L];
+        sB = new Matrix[L];
 
         int cnt = -1;
         for(auto itm : il)  Siz[++cnt] = itm;
         for (int i = 1;i < L;i++) {
             W[i] = MatrixAssignment(Siz[i], Siz[i-1], l, r);
             B[i] = MatrixAssignment(Siz[i], 1, l, r);
+            vW[i] = MatrixAssignment(Siz[i], Siz[i-1]);
+            vB[i] = MatrixAssignment(Siz[i], 1);
+            sW[i] = MatrixAssignment(Siz[i], Siz[i-1]);
+            sB[i] = MatrixAssignment(Siz[i], 1);
         }
     }
 
@@ -51,35 +66,39 @@ struct NNwork {
         dZ = new Matrix[L];
         dW = new Matrix[L];
         dB = new Matrix[L];
+        vW = new Matrix[L];
+        vB = new Matrix[L];
+        sW = new Matrix[L];
+        sB = new Matrix[L];
 
         for (int i = 0;i < L;i++)   Siz[i] = arr[i];
 
         for (int i = 1;i < L;i++) {
             W[i] = MatrixAssignment(Siz[i], Siz[i-1], l, r);
             B[i] = MatrixAssignment(Siz[i], 1, l, r);
+            vW[i] = MatrixAssignment(Siz[i], Siz[i-1]);
+            vB[i] = MatrixAssignment(Siz[i], 1);
+            sW[i] = MatrixAssignment(Siz[i], Siz[i-1]);
+            sB[i] = MatrixAssignment(Siz[i], 1);
         }
     }
 
     ~NNwork(){
-        delete [] Z, A, W, B, dZ, dW, dB;
+        delete [] Z, A, W, B, dZ, dW, dB, vW, vB, sW, sB;
     }
 
     Matrix Act(const Matrix &x) {
-        return logistic(x);
+        return tanh(x);
     }
 
     Matrix Act_D(const Matrix &x) {
-        return logistic_D(x);
+        return tanh_D(x);
     }
 
-
-
     Matrix ForwardPropagation(const Matrix &x) {
-        A[0] = Z[0] = x;
-        for (int l = 1; l < L; l++) {
-            Matrix B_pi = B[l], tmp = MatrixAssignment(W[l].row, A[l-1].col);
-            BroadCast(tmp, B_pi);
-            Z[l] = W[l] * A[l-1] + B_pi;
+        Z[0] = A[0] = x;
+        for (int l = 1; l < L; l++) {;
+            Z[l] = W[l] * A[l-1] + B[l](0, 0);
             A[l] = Act(Z[l]);
         }
         //std::cout << "F: ";  disp(A[L-1]);
@@ -87,14 +106,34 @@ struct NNwork {
     }
 
     void BackwardPropagation(const Matrix &y) {
-        dZ[L-1] = 2 % (log(1 + A[L-1]) - log(1 + y)) / (1 + A[L-1]);
+        // dZ[L-1] = 2 % (log(1 + A[L-1]) - log(1 + y)) / (1 + A[L-1]);
 
-        //dZ[L-1] = 2 % (A[L-1] - y) % Act_D(Z[L-1]);
+        dZ[L-1] = 2 % (A[L-1] - y) / y.row / y.col;
 
         for (int l = L-1; l > 0; l--) {
             dW[l] = dZ[l] * T(A[l-1]);
             dB[l] = sum(dZ[l], 1);
             if(l-1) dZ[l-1] = (T(W[l]) * dZ[l]) % Act_D(Z[l-1]);
+        }
+    }
+
+    void Adam(double lr = 0.1, double beta1 = 0.9, double beta2 = 0.999) {
+        cnt++;
+        for (int l = 0;l < L;l++) {
+            vW[l] = beta1 % vW[l] + (1 - beta1) % dW[l];
+            vB[l] = beta1 % vB[l] + (1 - beta1) % dB[l];
+
+            sW[l] = beta2 % sW[l] + (1 - beta2) % pow(dW[l]);
+            sB[l] = beta2 % sB[l] + (1 - beta2) % pow(dB[l]);
+
+            Matrix vW_ = vW[l] / (1 - pow(beta1, cnt));
+            Matrix vB_ = vB[l] / (1 - pow(beta1, cnt));
+
+            Matrix sW_ = sW[l] / (1 - pow(beta2, cnt));
+            Matrix sB_ = sB[l] / (1 - pow(beta2, cnt));
+
+            W[l] = W[l] - lr % vW_ / (sqrt(sW_) + 1e-8);
+            B[l] = B[l] - lr % vB_ / (sqrt(sB_) + 1e-8);
         }
     }
 
@@ -121,6 +160,7 @@ struct NNwork {
         cout << endl << endl;
         for(int i = 1; i < L;i++)   disp(W[i]);
         for(int i = 1; i < L;i++)   disp(B[i]);
+        // printf("%.8lf %.8lf\n");
         cout << Samples_X_ori.row << " " << Samples_X_ori.col << endl;  disp(Samples_X_ori);
         cout << endl;
         cout << Samples_Y_ori.row << " " << Samples_Y_ori.col << endl;  disp(Samples_Y_ori);
